9f9e84612bf126cc29c6cfd304d48a137e7e65fd
jaebin0719 <flxksldk@gmail.com>
2024-03-08 13:20:57 +0900

    feat : 프로젝트 부분 작업중

--- js/work.js
+++ js/work.js
@@ -1,23 +1,15 @@
 $(document).ready(function () {
-  let swiperSliderSimpleA = new Swiper(".slider-simple-a.swiper-container", {
+  var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 2,
+    spaceBetween: 50,
+    cssMode: true,
     navigation: {
-      nextEl: ".swiper-container.slider-simple-a .slider-next",
-      prevEl: ".swiper-container.slider-simple-a .slider-prev",
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
     },
     pagination: {
       el: ".swiper-pagination",
-      clickable: true,
     },
     loop: true,
-    grabCursor: true,
-    autoplay: {
-      delay: 5000,
-    },
-    spaceBetween: 20,
-    slidesPerView: "auto",
-
-    centeredSlides: true,
-    speed: 600,
-    // centeredSlidesBounds: true,
   });
 });
