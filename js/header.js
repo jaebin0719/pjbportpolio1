379dd1bbc2e06f28f86214120e0cf3132e20b775
jaebin0719 <flxksldk@gmail.com>
2024-03-14 18:07:27 +0900

    feat : 오류 수정

--- js/header.js
+++ js/header.js
@@ -1,16 +1,1 @@
-$(document).ready(function () {
-  $(window).on("scroll", function () {
-    var scrollPos = $(window).scrollTop();
-    $(".menu a").each(function () {
-      var currLink = $(this);
-      var refElement = $(currLink.attr("href"));
-      if (
-        refElement.position().top <= scrollPos &&
-        refElement.position().top + refElement.height() > scrollPos
-      ) {
-        $(".menu a").removeClass("active");
-        currLink.addClass("active");
-      }
-    });
-  });
-});
+$(document).ready(function () {});
