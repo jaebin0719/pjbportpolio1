ca21fba717510aae91950ba78392638ea041a6c2
jaebin0719 <flxksldk@gmail.com>
2024-03-15 17:56:58 +0900

    feat : header 메뉴바 생성

--- js/header.js
+++ js/header.js
@@ -1,1 +1,20 @@
-$(document).ready(function () {});
+$(document).ready(function () {
+  const menuBar = $(".menu-bar");
+  const menuItems = $(".menu li");
+  const menuBardiv = $(".menu-bar div");
+  let menuOpen = false;
+  menuItems.css({
+    display: "none",
+  });
+  menuBar.click(function () {
+    if (menuOpen) {
+      menuItems.fadeOut(200);
+    } else {
+      menuItems.fadeIn(200);
+    }
+    menuOpen = !menuOpen;
+  });
+  $(".menu-bar").on("click", function () {
+    $(this).toggleClass("open");
+  });
+});
