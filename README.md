5b9f0ae00b15ae78c7a781503d163b956e68202c
jaebin0719 <flxksldk@gmail.com>
2024-03-20 18:43:48 +0900

    feat : 메인페이지 반응형 완성(이미지 수정),서브페이지 반응형 해야함

--- README.md
+++ README.md
@@ -1,3 +1,1 @@
 # pjbportpolio1
-
-feat : 포트폴리오 프로젝트 생성
--- js/work.js
+++ js/work.js
@@ -4,6 +4,7 @@ $(document).ready(function () {
     slidesPerView: 2,
     spaceBetween: 50,
     cssMode: true,
+    loop: true,
     navigation: {
       nextEl: ".swiper-button-next",
       prevEl: ".swiper-button-prev",
@@ -12,6 +13,20 @@ $(document).ready(function () {
       el: ".swiper-pagination",
     },
     loop: true,
+    breakpoints: {
+      1025: {
+        slidesPerView: 2,
+      },
+      768: {
+        slidesPerView: 1.5,
+        centeredSlides: true,
+        spaceBetween: 30,
+      },
+      320: {
+        slidesPerView: 1,
+        centeredSlides: true,
+        spaceBetween: 30,
+      },
+    },
   });
-  //
 });
